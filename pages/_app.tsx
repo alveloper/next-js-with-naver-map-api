216851b113885479678b152cb43fd694ac272bea
Suyeon Kim <alveloper@gmail.com>
2021-12-14 21:09:58 +0900

    .

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,40 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+// pages/_app.tsx
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import * as React from "react";
+import Head from "next/head";
+import { AppProps } from "next/app";
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import { CacheProvider, EmotionCache } from "@emotion/react";
+import theme from "../styles/theme";
+import createEmotionCache from "../utils/createEmotionCache";
+// (1) Script import
+import Script from "next/script";
+
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache();
+
+interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache;
 }
 
-export default MyApp
+export default function MyApp(props: MyAppProps) {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+  return (
+    <CacheProvider value={emotionCache}>
+      {/* (2) Script 컴포넌트로 api 호출하기 */}
+      <Script
+        strategy="beforeInteractive"
+        src="https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=yvltix2hh3"
+      />
+      <Head>
+        <title>Change title in _app.tsx</title>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  );
+}
---
+++ utils/createEmotionCache.ts
@@ -0,0 +1,7 @@
+// utils/createEmotionCache.ts
+
+import createCache from "@emotion/cache";
+
+export default function createEmotionCache() {
+  return createCache({ key: "css" });
+}
